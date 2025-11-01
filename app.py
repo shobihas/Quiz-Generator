ec06ed889e9fd4cd8804a9fe4928b561f5703d75
shobihas <shobii1808@gmail.com>
2025-11-01 10:12:20 +0530

    commit

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 import streamlit as st
 import chains
-
 def quiz_generator_app():
     """
     Generates Quiz Generator App with Streamlit, providing user input and displaying output.
     """
     st.title("Lets generate a quiz ! 👋")
-
     with st.form("quiz_generator"):
         topic = st.text_input("Enter a topic for the quiz:")
         num_questions = st.number_input("Enter the number of questions:", min_value=1, value=5)
         submitted = st.form_submit_button("Submit")
-
         if submitted:
             response = chains.generate_quiz_chain(topic, num_questions)
             st.info(response)
-
 quiz_generator_app()
